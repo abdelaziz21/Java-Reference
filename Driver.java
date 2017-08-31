c34a73ea7a002a51504a24c31c52cddb2ebea7cd
abdelaziz21 <31153357+abdelaziz21@users.noreply.github.com>
2017-08-31 13:21:12 +0200

    Update Driver
    Improved functionality

--- Driver.java
+++ Driver.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 
+import java.awt.*;
+
 //Abdelaziz[************@gmail.com]
 /**
 * Overview: Use classes and methods to display output
@@ -27,6 +29,8 @@ import javax.swing.*;
 		Parent loop= new Parent();
 		//Crete new object of Parent class named flow
 		Parent flow= new Parent();
+		//Crete new object of Parent class named flow
+		Parent child= new Parent();
 		//Crete new object of Parent class named String
 		Parent string= new Parent();
 		//Crete new object of Parent class named array
@@ -35,8 +39,7 @@ import javax.swing.*;
 		Parent call= new Parent();
 		//Crete new object of Parent class named method
 		Parent tool= new Parent();
-		//Crete new object of Parent class named method
-		Parent media= new Parent();
+		
 		//System.out object is calling the println method for output
 		System.out.println("main: test="+test);
 		//scope object method calls
@@ -45,6 +48,7 @@ import javax.swing.*;
 		scope.printParamTest(60);
 		scope.printProtectedTest();
 		scope.printFinalTest();
+		scope.toString();
 		//loop object method calls
 		loop.forloop();
 		loop.whileloop();
@@ -54,6 +58,7 @@ import javax.swing.*;
 		flow.ifthen();
 		flow.switchstatement();
 		flow.ifthenelse();
+		//child object method calls
 		//string object method calls
 		string.StringTest();
 		string.CompareStrings();
@@ -65,18 +70,18 @@ import javax.swing.*;
 		call.format();
 		call.toString();
 		//tool object method calls
-		tool.Setter();
-		System.out.println(tool.Getter());
-		//media object method calls
-	    JFrame frame = new JFrame();
-			frame.setTitle("Picture");
+		tool.Setter("Abdelaziz");
+		System.out.println("My name is "+ tool.Getter());
+		tool.studentinfo();
+		
+		   JFrame frame = new JFrame();
+			frame.setTitle("Picure");
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			GraphicPanel panel = new GraphicPanel();
-			frame.getContentPane().add(panel);
+			frame.getContentPane().add(panel);		//	to do this
 			frame.pack();
-			frame.setSize(859, 524);			// set the size of the frame
+			frame.setSize(300, 250);			// set the size of the frame
 			frame.setVisible(true);
-		
 } 
 		
 	}
